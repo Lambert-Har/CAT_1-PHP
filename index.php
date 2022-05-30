a42f607709955e46e046e0782152f4a24d265dec
Lambert <lamberthar12@gmail.com>
2022-05-30 10:42:27 -0700

    Comit1

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ session_start();
                     <a href="">
                         <div class="bg-primary text-white d-flex ps-5 position-relative rounded mx-5 mt-5 mb-5 px-3 pt-2 border-2 shadow">
                             <i class="fab fa-facebook-f text-white position-absolute" style="left: 20px; top: 14px; font-size: 23px;" aria-hidden="true"></i>
-                            <h6>Continue with facebook</h6>
+                            <a href="https://www.facebook.com/">Continue with facebook</a>
                         </div>
                     </a>
 
--- logout.php
+++ logout.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
-    <title>Home</title>
+    <title>Dashboard</title>
 </head>
 <body>
     <section>
